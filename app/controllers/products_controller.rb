457881ad00a1dc303ac124f5cb7a9f66314d6caf
@suzuki.shota1 <suzuki.shota@algoage.co.jp>
2023-06-03 22:56:15 +0900

    products controllerの追加 & /products/ の URLパスの追加

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+  # GET /tasks or /tasks.json
+  def index
+    @products = Product.all
+  end
+
+  # GET /tasks/1 or /tasks/1.json
+  def show; end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @product = Product.find(params[:id])
+    @products = Product.all
+  end
+end
