4a4a109f9ac7ae8465ff17474b7538a77d61634b
@suzuki.shota1 <suzuki.shota@algoage.co.jp>
2023-06-04 11:30:19 +0900

    tasksコントローラーを利用しないため削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   # GET /tasks or /tasks.json
   def index
     @products = Product.all
   end
 
   # GET /tasks/1 or /tasks/1.json
-  def show; end
+  def show; 
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   resources :products
-  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
