ccdd71ff458bb43ab8cf294cba8e92ef50046875
@suzuki.shota1 <suzuki.shota@algoage.co.jp>
2023-06-09 16:20:01 +0900

    画像を登録するためのコーディングが完了

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  # 商品一覧の処理
+  # 商品一覧
   def index
     @products = Product.all
   end
@@ -8,26 +8,39 @@ class Admin::ProductsController < ApplicationController
     set_task
   end
 
-  # 商品作成の処理
+  # 商品登録
   def new
-    
+    @product = Product.new
   end
 
-  # 商品編集の処理
+  # 商品作成
+  def create
+    @product = Product.new(product_params)
+    @product.image.attach(params[:product][:image]) # 画像をアタッチする
+
+    if @product.save
+      redirect_to admin_product_path(@product), notice: '商品が登録されました。'
+    else
+      render :new
+    end
+  end
+
+  # 商品編集
   def edit
-    
   end
 
-  # 商品削除の処理
+  # 商品削除
   def destroy
-    
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_task
     @product = Product.find(params[:id])
     @products = Product.order(created_at: :desc).limit(4)
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image)
   end
 end
