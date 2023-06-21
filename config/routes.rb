ea54fde3451fbf9e1eaa9151a97e572b595bb4de
@suzuki.shota1 <suzuki.shota@algoage.co.jp>
2023-06-21 21:09:37 +0900

    リダイレクトの設定を追加/products/

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: redirect('/products')
+
   resources :products
 
   resource :cart
 
-  # add_item/:idのURLにアクセスがあった場合、cartsコントローラー(CartsController)のadd_itemアクションを呼び出す
-  # 名前付きadd_item_pathヘルパーを生成
   post 'add_item/:id', to: 'carts#add_item', as: 'add_item'
 
   namespace :admin do
