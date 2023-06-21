543351652d327d214e45fd37641be1eea5925c8f
@suzuki.shota1 <suzuki.shota@algoage.co.jp>
2023-06-21 21:05:09 +0900

    詳細画面は入力された数だけ数量を追加する

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,9 +5,10 @@ class CartsController < ApplicationController
   # カートに商品を追加する
   def add_item
     @product = Product.find(params[:id])
-    current_cart.add_item(@product.id)
-    
-    # Rails.logger.debug("controller:カートアイテム情報: #{current_cart.items.inspect}")
+    quantity = params[:quantity].present? ? params[:quantity].to_i : 1
+
+    # timesメソッドで、指定した回数だけ繰り返し処理を行うことで、カートに商品を追加する
+    quantity.times { current_cart.add_item(@product.id) }
     redirect_to products_path, notice: 'カートに商品を追加しました'
   end
 end
