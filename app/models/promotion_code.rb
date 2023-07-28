3fb982cf992f757f329273ff2821a35dcb249a45
Shota <wvp360gqanukcqhg@gmail.com>
2023-07-28 14:17:45 +0900

    promotion_codeの実装が完了

--- app/models/promotion_code.rb
+++ app/models/promotion_code.rb
@@ -1,2 +1,10 @@
 class PromotionCode < ApplicationRecord
+  # プロモーションコード
+  validates :code, presence: true, length: { is: 7 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  # 割引額
+  validates :discount_amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 1000 }
+  # 使用済みかどうか
+  validates :used, inclusion: { in: [true, false] }
+
+  # has_many :orders
 end
--- rails.sh
+++ rails.sh
@@ -1,36 +1,41 @@
 # 💻 チェックアウト機能実装
-# railsコマンドなど実行したコマンドをメモとして記録しておく
-# モデル追加
+## railsコマンドなど実行したコマンドをメモとして記録しておく
+## モデル追加
 docker-compose run --rm web rails generate model Order cart:references
 docker-compose run --rm web rails generate model OrderItem order:references product:references quantity:integer
 docker-compose run --rm web rails generate model Address order:references first_name:string last_name:string user_name:string email_name:string address1:string address2:string country:string prefectures:string post_code:string
 docker-compose run --rm web rails generate model Payment order:references card_name:string name_cart:string credit_number:string expiration:string cvv:string
 docker-compose run --rm web rails db:migrate
 
-# OrderItemテーブルのpriceカラムを追加
+## OrderItemテーブルのpriceカラムを追加
 docker-compose run --rm web rails generate migration AddPriceToOrderItems price:decimal
 docker-compose run --rm web rails db:migrate
 
-# Addressテーブルのcountryカラムを削除
+## Addressテーブルのcountryカラムを削除
 docker-compose run --rm web rails generate migration RemoveAddressFromAddress country:string
 docker-compose run --rm web rails db:migrate
 
-# mailerモデルの作成
+## mailerモデルの作成
 docker-compose run --rm web rails generate mailer OrderMailer
 
-# ロールバック
+## ロールバック
 docker-compose run --rm web rails db:rollback
 
-# heroku
+## heroku
 git push heroku feature/checkout\&purchase:main
 
 ・Heroku上でデータベースマイグレーションを実行する
 heroku run rake db:migrate
 
-# rubocop
+## rubocop
 docker-compose run --rm web bundle exec rubocop -A
 docker-compose run --rm web bin/htmlbeautifier
 
 # 💻 プロモーションコード機能実装
 docker-compose run --rm web rails g model PromotionCode code:string discount_amount:integer used:boolean
 docker-compose run --rm web rails db:migrate
+
+## rake
+docker-compose exec web rake -T
+docker-compose run --rm web rake -T
+docker-compose run --rm web rake promotion_code:generate
